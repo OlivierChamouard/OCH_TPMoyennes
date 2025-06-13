e5d93fa2a7b68fc90a4f2e77c0eb4a2ee21671e8
OlivierChamouard <olivier.chamouard@gmail.com>
2025-06-13 19:05:16 +0200

    Add files via upload

---
+++ Note.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HNI_TPmoyennes
+{
+    // Classes fournies par HNI Institut
+    class Note
+    {
+        public int matiere { get; private set; }
+        public float note { get; private set; }
+        public Note(int m, float n)
+        {
+            matiere = m;
+            note = n;
+        }
+    }
+}
